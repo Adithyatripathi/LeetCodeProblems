4bf0acb4ecddfcc595d748fe4a85424586e2ab3d
Adithyatripathi <148991165+Adithyatripathi@users.noreply.github.com>
2025-07-25 13:38:26 +0530

    postion.java

--- leetcode/arrays/postion.java
+++ leetcode/arrays/postion.java
@@ -1,11 +1,12 @@
 class postion {
     public int searchInsert(int[] nums, int target) {
-        for (int i = 0; i < nums.length; i++) { // Use i < nums.length to avoid out-of-bounds error
-            if (nums[i] >= target) { // If target is found or nums[i] is larger, return the index
-                return i;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] = target) { 
+             return i;
+                break;
             }
         }
-        // If target is greater than all elements, return the last index + 1
-        return nums.length;
+      
+        return -1;
     }
 }
